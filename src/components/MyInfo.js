535d5609b55f959a18a7ac9af8757ed8aa4c914c
Nupur M <NupurM@live.com>
2019-06-20 13:21:44 -0700

    Create separate function file

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Nupur Maheshwari</h1>
+            <p>I am trying to learn React with the help of Bob Ziroll.</p>
+            <ol>
+                <li>Hawaii</li>
+                <li>San Francisco</li>
+                <li>Downtown</li>
+            </ol>
+        </div>
+    );
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import MyInfo from './components/MyInfo';
 
 ReactDOM.render(<MyInfo />, document.getElementById('root'));
 
-function MyInfo() {
-    return (
-        <div>
-            <h1>Nupur Maheshwari</h1>
-            <p>I am trying to learn React with the help of Bob Ziroll.</p>
-            <ol>
-                <li>Hawaii</li>
-                <li>San Francisco</li>
-                <li>Downtown</li>
-            </ol>
-        </div>
-    );
-}
