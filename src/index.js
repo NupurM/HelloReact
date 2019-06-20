912f1c461659392ebbbb81c1e6e856001b039398
Nupur M <NupurM@live.com>
2019-06-20 13:02:17 -0700

    Created my own index file and function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MyInfo />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function MyInfo() {
+    return (
+        <div>
+            <h1>Nupur Maheshwari</h1>
+            <p>I am trying to learn React with the help of Bob Ziroll.</p>
+            <ol>
+                <li>Hawaii</li>
+                <li>San Francisco</li>
+                <li>Downtown</li>
+            </ol>
+        </div>
+    );
+}
