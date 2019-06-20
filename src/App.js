0334172ec3edd62bf178feeae5285c3e8df1162f
Nupur M <NupurM@live.com>
2019-06-20 12:24:39 -0700

    Say Hello World!

--- src/App.js
+++ src/App.js
@@ -6,18 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>Hello World!</h3>
+        <p>Welcome to my React demo.</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
