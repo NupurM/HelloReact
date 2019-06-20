d4c9fcfa83b71c6a49f1c3333853053da80efa1d
Nupur M <NupurM@live.com>
2019-06-20 14:21:54 -0700

    Add dynamic header and header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-function Header(){
-    return(
-        <header>This is my header</header>
+function Header() {
+    const firstname = 'Nupur';
+    const lastname = 'Maheshwari';
+
+    return (
+        <header className="navbar">
+            <p>{firstname +' '+ lastname} here! I am trying to learn React with the help of Bob Ziroll.</p>
+        </header>
     );
 }
 
